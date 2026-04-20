27ae6731fd70e1ecbeb78d5b53ca6e690c434dfc
MD Jakuar Hossain Ovi <148855922+JakuarOvi@users.noreply.github.com>
2026-04-20 07:24:50 +0600

    Delete ParkManager_Fixed directory

--- ParkManager_Fixed/backend/parking/auth_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_view, register_view, change_password
-
-urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
-    path('change-password/', change_password, name='change-password'),
-]
